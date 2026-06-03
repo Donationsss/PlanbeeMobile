ac5426993ff41edf95e7f8c5d1b3dd64c5607a90
Donassan <enzo.donassan@gmail.com>
2026-06-03 12:34:10 -0300

    arrumando a agenda

--- planbeemobile/lib/agenda.dart
+++ planbeemobile/lib/agenda.dart
@@ -12,7 +12,48 @@ class Agenda extends StatefulWidget {
   State<Agenda> createState() => _AgendaState();
 }
 
+class Dia {
+  final String dia;
+
+  Dia({
+    required this.dia,
+  });
+}
+
+final List<Dia> Dias = [
+  Dia(dia: "1"),
+];
+
 class _AgendaState extends State<Agenda> {
+
+  void _mostrarDetalhes(Dia dias) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                dias.dia,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,29 +66,60 @@ class _AgendaState extends State<Agenda> {
       backgroundColor: const Color(0xFFFFF9CF),
       body: Center( // Centraliza tudo na tela
         child: Container(
-          width: 350,
-          height: 400,
-          padding: EdgeInsets.all(14),
-          decoration: BoxDecoration(
-            color: Color(0xFF2D2D2D),
-            borderRadius: BorderRadius.circular(23),
-          ),
-
-          child: Container(
+            width: 400,
+            height: 400,
+            padding: EdgeInsets.all(14),
             decoration: BoxDecoration(
-              color: Color(0xFFFFDA5E),
+              color: Color(0xFF2D2D2D),
               borderRadius: BorderRadius.circular(23),
             ),
-            child: GridView.count(
-              crossAxisCount: 7,
-              childAspectRatio: 0.5,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 15,
-              children: [
-                
-              ]
-            ),
-          )
+
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xFFFFDA5E),
+                borderRadius: BorderRadius.circular(23),
+              ),
+              child: GridView.count(
+                padding: EdgeInsets.all(8),
+                crossAxisCount: 7,
+                childAspectRatio: .6,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 15,
+                children: List.generate(Dias.length, (index) {
+                  final dias = Dias[index];
+
+                  return InkWell(
+                    onTap: () {
+                      _mostrarDetalhes(dias);
+                    },
+                    child: Card(
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      clipBehavior: Clip.antiAlias, // corta qualquer overflow
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: EdgeInsets.all(8),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    dias.dia,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            )
         ),
       ),
       bottomNavigationBar: BottomBar(),
